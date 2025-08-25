bcbf9aa24f6093860b76906cdcf162bbc75be0ff
Pierre Villemot <pierre.villemot@ocamlpro.com>
2025-08-25 02:07:21 +0200

    update

--- vm/mirageos/default.nix
+++ vm/mirageos/default.nix
@@ -1,35 +1,42 @@
 { pkgs, ... }:
 
+let
+  ocamlPackages = pkgs.ocamlPackages;
+in
 pkgs.mkShell {
-  buildInputs = with pkgs; [
+  buildInputs = (with pkgs; [
     qemu
     qemu_xen
     qemu-utils
-    grub2_pvgrub_image
     xen
-  ];
+  ]) ++ (with ocamlPackges; [
+    ocaml
+    dune_3
+    mirage
+    mirage_xen
+  ]);
 
-  shellHook = ''
-    BRIDGE="xenbr"
+  # shellHook = ''
+  #   BRIDGE="xenbr"
 
-    cleanup() {
-      sudo xl destroy alpine
-    }
+  #   cleanup() {
+  #     sudo xl destroy mirageos
+  #   }
 
-    trap cleanup EXIT
+  #   trap cleanup EXIT
 
-    cat > ./vm/alpine/vm.cfg << EOF
-    name='alpine'
-    memory='2048'
-    vcpus=2
-    type='pv'
-    kernel='${pkgs.grub2_pvgrub_image}/lib/grub-xen/grub-x86_64-xen.bin'
-    disk=[ './vm/alpine/disk.qcow2,qcow2,xvda,w' ]
-    boot='d'
-    vif = [ 'mac=00:16:3e:00:00:00,bridge=$BRIDGE' ]
-    device_model_override='/run/current-system/sw/bin/qemu-system-i386'
-    EOF
+  #   cat > ./vm/mirageos/vm.cfg << EOF
+  #   name='mirageos'
+  #   memory='2048'
+  #   vcpus=2
+  #   type='pv'
+  #   kernel='${pkgs.grub2_pvgrub_image}/lib/grub-xen/grub-x86_64-xen.bin'
+  #   disk=[ './vm/alpine/disk.qcow2,qcow2,xvda,w' ]
+  #   boot='d'
+  #   vif = [ 'mac=00:16:3e:00:00:00,bridge=$BRIDGE' ]
+  #   device_model_override='/run/current-system/sw/bin/qemu-system-i386'
+  #   EOF
 
-    sudo xl create ./vm/alpine/vm.cfg -c
-  '';
+  #   sudo xl create ./vm/mirageos/vm.cfg -c
+  # '';
 }
